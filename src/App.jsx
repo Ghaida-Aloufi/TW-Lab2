42909d828635eccca2f3c759e992cf1dd66d201e
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-26 15:52:54 +0300

    add lab2

---
+++ src/App.jsx
@@ -0,0 +1,135 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+
+<div className="p-5 bg-cover bg-center h-[50vh]" style={{   backgroundImage: 'url("https://early-wildflower-281.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffb56a855-0c74-4a0d-84dc-3fb7c76ae0b3%2F55d7c998-fd67-4237-a21a-c7f9699e229a%2FScreen_Shot_2023-11-26_at_10.49.03_AM_cleanup.png?table=block&id=fff185a2-dff1-81af-bc73-de1e521c36bc&spaceId=fb56a855-0c74-4a0d-84dc-3fb7c76ae0b3&width=1420&userId=&cache=v2")' }}>
+
+
+        <div className="flex flex-col items-start justify-center h-full ">
+          <h4 className="text-white text-2xl font-bold mb-4">FreshGoods</h4>
+          <h1 className="text-white text-5xl max-w-lg">
+            Concisely describe your product or service
+          </h1>
+          <p className="text-white text-base mt-4 max-w-md">
+            No need to get clever. Tell people exactly what you're offering,
+            then use this space to communicate your key value proposition.
+          </p>
+          <button className="mt-6 bg-yellow-500 text-black py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+      
+      </div>
+
+    <div className='flex flex-col justify-center items-center h-[70vh]  justify-around'>
+      
+  <div className='flex flex-col justify-center items-center min-h-[70vh] space-y-8 md:space-y-4'>
+  <div className='flex flex-col items-center text-center'>
+    <h1 className='text-3xl md:text-5xl font-sans w-[90vw] md:w-[60vw]'>
+      Here are some of the benefits of your offer
+    </h1>
+    <h3 className='w-[90vw] md:w-[70vw]'>
+      Explain what makes your product or service great. Talk about features in a way that highlights the real value people get out of them.
+    </h3>
+  </div>
+
+  <div className='flex flex-wrap justify-center gap-4 w-[90vw] md:w-[70%]'>
+    <div className='flex flex-col items-center text-center w-full md:w-1/3'>
+      <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/b41bc87d-freshgoods-benefit2-icon_1000000000000000000028.png" alt="" className='w-16 h-16 mb-2'/>
+      <p className='font-bold'>Benefit 1</p>
+      <p>For example, restaurants and bakeries could outline the health benefits of their all natural ingredients.</p>
+    </div>
+    <div className='flex flex-col items-center text-center w-full md:w-1/3'>
+      <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/6622ad60-freshgoods-benefit3-icon_1000000000000000000028.png" alt="" className='w-16 h-16 mb-2'/>
+      <p className='font-bold'>Benefit 2</p>
+      <p>Florists and other small retailers might use this space to describe how their products make delightful, one-of-a-kind sifts</p>
+    </div>
+    <div className='flex flex-col items-center text-center w-full md:w-1/3'>
+      <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/3a18a275-freshgoods-benefit1-icon_1000000000000000000028.png" alt="" className='w-16 h-16 mb-2'/>
+      <p className='font-bold'>Benefit 3</p>
+      <p>You could also add buttons to this section and share links to other products or available delivery methods.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<div className='flex w-full items-center justify-center'>
+  <div className='w-full h-[70vh]' style={{ backgroundImage: 'url("https://early-wildflower-281.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffb56a855-0c74-4a0d-84dc-3fb7c76ae0b3%2Fb065d811-70a9-454e-947e-699ec2801a37%2FScreen_Shot_2023-11-26_at_11.01.17_AM_cleanup.png?table=block&id=fff185a2-dff1-8155-8342-ca7bab2c8a64&spaceId=fb56a855-0c74-4a0d-84dc-3fb7c76ae0b3&width=860&userId=&cache=v2")' }}>
+
+    <div className='flex flex-col items-center justify-center h-full p-4 md:justify-around'>
+      
+      <div className='flex flex-col items-center justify-around h-[45vh] p-2 text-center'>
+        <h3 className='text-white text-sm md:text-base'>What our customers say</h3>
+        <h1 className='text-white w-[80vw] md:w-[70vh] text-xl md:text-3xl font-serif text-center'>
+          Share a real testimonial that hits some of your benefits (but isn't too sales-y).
+        </h1>
+      </div>
+
+      <div className='flex flex-col gap-2 items-center justify-between mt-4 md:mt-0'>
+        <img className='w-[90%] md:w-[80%]' src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/1a1f4659-freshgoods-testimonial_1000000000000000000028.png" alt="" />
+        <h2 className='text-white text-xs md:text-base'>REAL NAME LOCATION</h2>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div className='flex flex-wrap items-center justify-around w-full bg-[#f5f5f5] h-auto md:h-[80vh] p-4'>
+  <div className='flex flex-col justify-center items-center text-center w-full md:w-[30vh] mb-6 md:mb-0'>
+    <img className='w-[80%] md:w-auto' src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/f55dfcfa-freshgoods-feature3_10go0b40go09e00000601o.jpeg" alt="" />
+    <h1 className='font-bold mt-3'>Feature 3</h1>
+    <h2 className='w-[80%] md:w-[40vh] text-center'>Alternatively, you could use this section to address some frequently asked questions.</h2>
+  </div>
+  
+  <div className='flex flex-col justify-center items-center text-center w-full md:w-[30vh] mb-6 md:mb-0'>
+    <img className='w-[80%] md:w-auto' src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/bf92a2f6-freshgoods-feature2_10go0ku0go09e00009e01o.jpg" alt="" />
+    <h1 className='font-bold mt-3'>Feature 2</h1>
+    <h2 className='w-[80%] md:w-[40vh] text-center'>Is there a pain point that your product or service resolves? Tell visitors about it here.</h2>   
+  </div>
+  
+  <div className='flex flex-col justify-center items-center text-center w-full md:w-[30vh]'>
+    <img className='w-[80%] md:w-auto' src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/6765ae81-freshgoods-feature1_10go0b20go09e00000001o.jpg" alt="" />
+    <h1 className='font-bold mt-3'>Feature 1</h1>
+    <h2 className='w-[80%] md:w-[40vh] text-center'>Talk about some of the details of your offer with a focus on the value people get back.</h2> 
+  </div>
+</div>
+
+<div className='flex flex-wrap justify-center items-center gap-8 p-4 h-auto md:h-[80vh]'>
+  <img 
+    className='w-[90%] md:w-auto md:h-auto' 
+    src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/ef9d9b9f-freshgoods-about_100000000000000000001o.jpg" 
+    alt="" 
+  />
+  <div className='flex flex-col w-[90%] md:w-[70vh] h-auto md:h-[25vh] justify-center items-center space-y-4 md:space-y-0'>
+    <h1 className='text-2xl md:text-5xl font-sans text-center'>
+      Some more information about your business
+    </h1>
+    <h2 className='text-sm md:text-base text-center'>
+      Share a little about yourself as a business owner, or maybe describe what makes your product or service unique. Give visitors one more reason to care about your offer and want to buy from you.
+    </h2>
+  </div>
+</div>
+
+
+<div className='flex flex-col justify-center gap-4 items-center h-auto md:h-[50vh] w-full bg-[#f5f5f5] p-4'>
+  <h1 className='text-2xl md:text-5xl w-full md:w-[50vh] text-center'>
+    End with one final call to action
+  </h1>
+  <h2 className='text-sm md:text-base w-full md:w-[80vh] text-center'>
+    This is your last shot at converting someone. Remind them about the benefits of your offer and encourage them to act now.
+  </h2>
+  <button className="mt-4 md:mt-6 bg-yellow-500 text-black py-2 px-4 rounded">
+    Order Now
+  </button>
+</div>
+
+    </>
+  )
+}
+
+export default App
